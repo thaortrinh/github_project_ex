1af7dc4beb6715eea6b1e11449033300818f0636
TRINH <nghthtr5536@gmail.com>
2023-12-11 21:55:28 +0700

    adding file.

---
+++ src/GitHubProject.java
@@ -0,0 +1,20 @@
+public class GitHubProject {
+    public static void main(String[] args) {
+        System.out.println("This is my GitHub project.");
+
+//  •	Lý thuyết
+//  -	Repo (github repository): 1 dự án
+//      + local: repo trong máy
+//      + remote: nằm trên máy chủ, có thể pull về được, dùng git pull git push để đồng bộ source code
+//  -	Commit: sửa code, thay đổi code => 1 commit, được lưu vào repo
+//  -	Branch: 1 nhánh master, mỗi chức năng là mỗi branch
+//  •	Github: code chung, lưu trữ repo trên mạng
+//  •	Lệnh cơ bản
+//  -	Git init: tạo repo trong máy
+//  -	Git clone: lấy trên mạng về
+//  -	Git pull: đồng bộ từ trên mạng về máy (khi có ai đó khác chỉnh sửa code -> cần update lại)
+//  -	Git add, git commit: thêm file, sửa code trong repo trong máy
+//  -	Git push: đồng bộ từ máy lên trên mạng
+//  -	Git log, git status: check trạng thái của file như nào");
+    }
+}
